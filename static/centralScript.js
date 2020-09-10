f5eb36268c642d2ba15a2a5d222a057056e6842e
unknown <nicolopinci.1997@gmail.com>
2020-09-10 18:23:58 +0200

    Compatibility set also for sendData

--- static/centralScript.js
+++ static/centralScript.js
@@ -13,9 +13,14 @@ function retrieveData() {
 		var doc = obj.contentDocument; // get the inner DOM
 	setTimeout(function() {
 		if(doc!=null && doc!=undefined) {
-			var el = doc.getElementById("sendData"); // assuming the embedded document has such an element
-			el.addEventListener("click", sendData, false);
-			console.log("Added event listener");
+            var el = doc.getElementById("sendData"); // assuming the embedded document has such an element
+            if (el == null) {
+                retrieveData();
+            }
+            else {
+                el.addEventListener("click", sendData, false);
+                console.log("Added event listener");
+            }
 		}
 		else {
 			retrieveData();
