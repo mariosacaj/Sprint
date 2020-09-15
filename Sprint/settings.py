eff33e3087a9a783f7dd2af25b79d175f61f4b2c
mskx4 <mskx4@outlook.com>
2020-09-15 03:31:31 +0200

    moved compare.html in proper place

--- Sprint/settings.py
+++ Sprint/settings.py
@@ -54,8 +54,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates'
         ,
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +118,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-PATH_FILES = os.path.join(BASE_DIR, 'sprint_files/')
+PATH_FILES = os.path.join(BASE_DIR, 'session_files/')
 
 MODEL_DIR = os.path.join(BASE_DIR, 'model/')
 
